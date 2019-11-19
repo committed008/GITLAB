2830ce588efcf4a422fce34906fb852f9bceb776
committed008 <kirati29.03@gmail.com>
2019-11-19 17:37:44 +0700

    edit bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[5], answer[10];
+{ char Name[60], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your first name? ");
     scanf("%s", Name);
     do
     {
